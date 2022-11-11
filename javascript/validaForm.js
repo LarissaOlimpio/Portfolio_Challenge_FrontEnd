185c8f1149b48d72d587ba447b0892800f4d1268
Larissa Olimpio <lrsolimpio@gmail.com>
2022-11-11 17:46:19 -0300

    consumindo uma api

--- javascript/validaForm.js
+++ javascript/validaForm.js
@@ -1,6 +1,10 @@
 export function valida(input){
     const tipoDeInput = input.dataset.tipo
 
+    if (validaCep[tipoDeInput]){
+        validaCep[tipoDeInput](input)
+    }
+
     if(input.validity.valid){
         input.parentElement.classList.remove('.contato__input__container--invalido')
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
@@ -9,11 +13,14 @@ export function valida(input){
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput,input)
     }
 }
-
+const validaCep = {
+    cep:input => buscarCep(input)
+}
 
 const tiposDeErro = [
     'valueMissing',
     'typeMismatch',
+    'patternMismatch',
     'customError'
 ]
 const mensagemDeErro={
@@ -26,6 +33,20 @@ const mensagemDeErro={
     },
     assunto:{
         valueMissing:'O campo de assunto não pode estar vazio.'
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é válido.',
+        customError: 'Não foi possível buscar o CEP.'
+    },
+    logradouro: {
+        valueMissing: 'O campo de logradouro não pode estar vazio.'
+    },
+    cidade: {
+        valueMissing: 'O campo de cidade não pode estar vazio.'
+    },
+    estado: {
+        valueMissing: 'O campo de estado não pode estar vazio.'
     }
 }
 
@@ -38,3 +59,44 @@ function mostraMensagemDeErro(tipoDeInput,input){
     })
     return mensagem
 }
+//Buscando CEP utilizando uma Api
+function buscarCep(input){
+    const cep = input.value.replace(/\D/g,'')//trocar o que não for número por vazio
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method:'GET',
+        mode:'cors',
+        headers:{
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+    //verificando se o input esta devidamente preenchido
+    if(!input.validity.patternMismatch && !input.validity.valueMissing){
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro){
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCep(data)
+                return
+                    
+            }
+        )
+    }
+
+}
+//Preenchendo os campos de endereço a partir do CEP informado
+function preencheCamposComCep(data){
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
+}
+    
